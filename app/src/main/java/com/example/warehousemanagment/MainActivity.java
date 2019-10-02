8111583cccb8eed27f09f4859dc17b7c51d147bf
shadi <shadi.othman@hotmail.com>
2019-10-02 22:34:04 +0300

    initial

--- app/src/main/java/com/example/warehousemanagment/MainActivity.java
+++ app/src/main/java/com/example/warehousemanagment/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.warehousemanagment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +12,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    public void buttonAction(View view) {
-
-        Intent intent;
-
-        if (view.getId() == R.id.btn_add_record){
-
-            intent = new Intent(this, ProductsActivity.class);
-            startActivity(intent);
-        }
-        if (view.getId() == R.id.btn_delete_record){
-            return;
-        }
-    }
 }
