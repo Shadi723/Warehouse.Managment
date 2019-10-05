d91052d13edd13f4559026d0e3f3e430307ea1ff
Ali Daoud <ali.daoud08@outlook.sa>
2019-10-05 19:36:15 +0300

    share project on firebase and set add product method

--- app/src/main/java/com/example/warehousemanagment/MainActivity.java
+++ app/src/main/java/com/example/warehousemanagment/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.warehousemanagment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void get(){
 
+
+    public void buttonAction(View view) {
+
+        Intent intent;
+
+        if (view.getId() == R.id.btn_add_record){
+
+            intent = new Intent(this, ProductsActivity.class);
+            startActivity(intent);
+        }
+        if (view.getId() == R.id.btn_delete_record){
+            return;
+        }
     }
 }
